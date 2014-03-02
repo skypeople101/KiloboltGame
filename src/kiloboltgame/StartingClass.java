6438998652fd6dd96a9394f877b794c827099c45
skypeople101 <shuvrodeepdutta@gmail.com>
2014-03-02 17:01:57 +0530

    this is basic coding done in eclipse ADT
    now it creates an Applet window and displays the robot image. robot
    image has to be in bin/data folder to be properly read by jvm

---
+++ src/kiloboltgame/StartingClass.java
@@ -0,0 +1,197 @@
+package kiloboltgame;
+
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.net.URL;
+
+
+public class StartingClass extends Applet implements Runnable, KeyListener {
+	
+	private Robot robot;
+	//double buffering
+	private Image image, character;
+	private Graphics second;
+	private URL base;
+	
+	
+	
+	@Override
+	public void init (){
+		
+		setSize(800,480);
+		setBackground(Color.BLACK);
+		setFocusable(true);
+		addKeyListener(this);
+		Frame frame = (Frame) this.getParent().getParent();
+		frame.setTitle("Q-bot Alpha");
+		
+		
+		
+		try {
+			
+			base = getDocumentBase();
+			//System.out.println(base + " end");
+		}
+		catch (Exception e) {
+			
+			//TODO:Handle Exception
+			
+		}
+		
+		//image setup
+		character = getImage(base,"data/character.png");
+		
+		
+	}
+	
+	@Override
+	
+	public void start() {
+		
+		
+		robot = new Robot();
+		Thread thread = new Thread(this);
+		thread.start();
+		
+		
+	}
+	
+	
+	@Override
+	
+	public void stop() {
+		
+		super.stop();
+		
+	}
+	
+	@Override
+	
+	public void destroy() {
+		
+		super.destroy();
+		
+	}
+	
+	@Override
+	public void run() {
+		
+		while (true){
+			
+			robot.update();
+			
+			repaint();
+			try {
+				Thread.sleep(17);
+				
+			}
+			
+			catch (InterruptedException e ) {
+				e.printStackTrace();
+				
+			}
+		}
+	}
+
+	@Override
+	public void update (Graphics g){
+		
+		if(image == null) {
+			
+			image = createImage(this.getWidth(),this.getHeight());
+			second = image.getGraphics();
+		}
+		
+		second.setColor(getBackground());
+		second.fillRect(0, 0, getWidth(), getHeight());
+		second.setColor(getForeground());
+		
+		paint(second);
+		
+		g.drawImage(image, 0 ,0,this);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		
+		g.drawImage(character, robot.getCenterX()-61, robot.getCenterY()-63, this);
+	}
+	
+	
+	
+	
+	
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+		
+		switch(e.getKeyCode()){
+		
+		case KeyEvent.VK_UP:
+			break;
+			
+		case KeyEvent.VK_LEFT:
+			
+			robot.moveLeft();
+			break;
+			
+		case KeyEvent.VK_DOWN:
+			break;
+			
+		case KeyEvent.VK_RIGHT:
+			robot.moveRight();
+			break;
+			
+			
+		case KeyEvent.VK_SPACE:
+			robot.jump();
+			break;
+			
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+		switch (e.getKeyCode()) {
+		
+		case KeyEvent.VK_UP:
+			break;
+			
+		case KeyEvent.VK_LEFT:
+			robot.stop();
+			break;
+			
+		case KeyEvent.VK_DOWN:
+			break;
+			
+		case KeyEvent.VK_RIGHT:
+			robot.stop();
+			break;
+			
+			
+		case KeyEvent.VK_SPACE:
+			break;
+			
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+
+}
